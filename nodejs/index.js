ffbe7e193be74e52adc66390bad2e4af7c00d8c9
wakamoe <hexakill095793@gmail.com>
2026-01-10 15:20:37 +0800

    updated cors

--- nodejs/index.js
+++ nodejs/index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://jabmeats.netlify.app"
+    "https://jabmeats.netlify.app",
+    "https://jab-meats.vercel.app"
   ],
   credentials: true
 }));
