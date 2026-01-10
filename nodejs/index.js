73b710ac5e90e2e7ec2caa74f24e1257eb4a811d
wakamoe <hexakill095793@gmail.com>
2026-01-10 14:45:11 +0800

    updated port

--- nodejs/index.js
+++ nodejs/index.js
@@ -10,7 +10,7 @@ import inventoryCountRouter from "./routes/inventory.js"
 
 
 const app = express();
-const PORT = process.env.nodejs_port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
